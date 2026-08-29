fffcd0b2a4c068bf21bdc96e399c399f34572b06
Ahmad Ali <ahmadalibusiness0071@gmail.com>
2026-08-30 03:57:28 +0500

    Fix server.js to use ES modules

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-﻿const http = require('http');
-const fs = require('fs');
-const path = require('path');
+﻿import http from 'http';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const PORT = process.env.PORT || 3000;
 const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
   res.end(html);
 }).listen(PORT, () => console.log(`Privacy policy running on port ${PORT}`));
